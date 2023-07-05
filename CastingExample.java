6447c0ce6e51f710980d62382126284ffc9010df
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-05 12:20:16 +0900

    Create CastingExample.java

---
+++ CastingExample.java
@@ -0,0 +1,12 @@
+package sec02.exam;
+
+public class Example05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double doubleValue = 3.14;
+		int intValue = (int)doubleValue;
+		System.out.println(intValue);
+	}
+
+}
