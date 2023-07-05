813f356ab4f1ec07e755d05d3832bcc6cf3c1792
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-05 12:18:41 +0900

    Create LogicalTypeExample.java

---
+++ LogicalTypeExample.java
@@ -0,0 +1,16 @@
+package sec02.exam;
+
+public class Example03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean stop =true;
+		if(stop) {
+			System.out.println("중지됨");
+		}
+		else {
+			System.out.println("실행됨");
+		}
+	}
+
+}
