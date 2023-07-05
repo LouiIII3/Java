0591a80d84c6b87eacf483f771a29e2b4a2ff434
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-05 12:19:55 +0900

    Create PromotionExample.java

---
+++ PromotionExample.java
@@ -0,0 +1,19 @@
+package sec02.exam;
+
+public class Example04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Byte byteValue = 10;
+		int intValue = byteValue;
+		System.out.println(intValue);
+		
+		//char 에서 int형으로
+		char charValue = 'B';
+		int num = charValue;
+		System.out.println(num);
+	}
+
+}
