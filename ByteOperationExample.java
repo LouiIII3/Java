f341fe988b60ffb0baf21f1ebe99837e772b60c1
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-05 12:20:47 +0900

    Create ByteOperationExample.java

---
+++ ByteOperationExample.java
@@ -0,0 +1,14 @@
+package sec02.exam;
+
+public class Example06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte x = 10;
+		byte y = 20;
+		int result = x+y;
+		
+		System.out.println(result);
+	}
+
+}
