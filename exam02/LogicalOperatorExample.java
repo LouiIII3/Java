5b2c4a65b1e61f73a15f2bf739996565cdd21436
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-06 11:21:47 +0900

    Create LogicalOperatorExample.java

---
+++ exam02/LogicalOperatorExample.java
@@ -0,0 +1,18 @@
+package sec03.exam01;
+
+public class LogicalOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 20;
+		if((x == 10) && (y == 20)) {
+			System.out.println("(x == 10) && (y == 20) 구문은 맞습니다.");
+		}
+		
+		if((x == 10) || (y == 10)) {
+			System.out.println("(x == 10) || (y == 10) 구문은 맞습니다.");
+		}
+	}
+
+}
