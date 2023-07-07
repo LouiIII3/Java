444ac767ba5161827124653a6d0a4388c17aab41
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-07 14:25:32 +0900

    Create ForMultiplicationExample.java

---
+++ sec04/ForMultiplicationExample.java
@@ -0,0 +1,13 @@
+package sec04.exam01;
+
+public class ForMultiplicationExample {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=9; i++) {
+			for(int j = 1; j <=9; j++) {
+				System.out.println(i + "x" + j + "=" + i*j);
+			}
+		}
+	}
+
+}
