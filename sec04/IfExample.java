9dbd774c92a552eb44d7982f16c6892ca23ccd9b
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-07 14:26:22 +0900

    Create IfExample.java

---
+++ sec04/IfExample.java
@@ -0,0 +1,17 @@
+package sec04.exam01;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int score = 93;
+		
+		if(score>=90) {
+			System.out.println("A등급 입니다.");
+			}
+		
+		if(score < 90) {
+			System.out.println("B등급 입니다.");
+		}
+	}
+
+}
