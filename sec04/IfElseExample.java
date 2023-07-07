4e19b25d8b129f2301b6e1f8a866664460a2fbb1
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-07 14:25:58 +0900

    Create IfElseExample.java

---
+++ sec04/IfElseExample.java
@@ -0,0 +1,18 @@
+package sec04.exam01;
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		int score = 85;
+		
+		if(score>=90) {
+			System.out.println("A등급 입니다.");
+			}
+		
+		else {
+			System.out.println("B등급 입니다.");
+		}
+		
+	}
+
+}
