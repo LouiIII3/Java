57859310c1fa97f9f65c84fcb0be51353f603a69
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-07 14:25:46 +0900

    Create ForSumFrom1To100Examle.java

---
+++ sec04/ForSumFrom1To100Examle.java
@@ -0,0 +1,16 @@
+package sec04.exam01;
+
+public class ForSumFrom1To100Examle {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=1; i<=100; i++) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
