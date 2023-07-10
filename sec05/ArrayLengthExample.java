8e6d17a49ce5542ca27fe72275b40917350bafce
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-10 13:34:24 +0900

    Update ArrayLengthExample.java

--- sec05/ArrayLengthExample.java
+++ sec05/ArrayLengthExample.java
@@ -3,7 +3,6 @@ package sec05.exam01;
 public class ArrayLengthExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[] scores = { 83, 90 ,97 };
 		int sum = 0;
 		for(int i=0; i<scores.length; i++) {
