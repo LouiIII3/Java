6c73273e57b2dfb52b5bd99df1a72df6d40dec08
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-11 14:15:53 +0900

    Add files via upload

---
+++ sec06/KoreaExample.java
@@ -0,0 +1,12 @@
+package sec06.exam01;
+
+public class KoreaExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Korean k1 =new Korean("전우치", "123456-7890");
+		System.out.println(k1.name);
+		System.out.println(k1.ssn);
+	}
+
+}
---
+++ sec06/StudentExample.java
@@ -0,0 +1,11 @@
+package sec06.exam01;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 =new Student();
+		System.out.println("s1이 객체를 참조합니다.");
+	}
+
+}
