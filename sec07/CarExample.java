d0aa5cca185c8cc0e591b59f8995c0c024fafbbf
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-12 18:53:00 +0900

    Add files via upload

---
+++ sec07/CarExample.java
@@ -0,0 +1,13 @@
+package sec06.exam01;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car myCar3 = new Car("택시", "검정");
+		System.out.println(myCar3.company);
+		System.out.println(myCar3.model);
+		System.out.println(myCar3.color);
+	}
+
+}
