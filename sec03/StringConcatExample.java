5663b6393a567417a45cfaabaa789aaefa9121f1
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-16 21:20:40 +0900

    Update StringConcatExample.java

--- sec03/StringConcatExample.java
+++ sec03/StringConcatExample.java
@@ -3,7 +3,7 @@ package sec03.exam01;
 public class StringConcatExample {
 
 	public static void main(String[] args) {
-		String str1 = "혼공학습단" + "10기";
+		String str1 = "혼공학습단" + " 10기";
 		String str2 = str1 + " Java";
 		System.out.println(str2);
 	}
