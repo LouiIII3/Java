fdcdb282bf4f44355bbd7ad904d0d6255a4c0869
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-21 10:28:42 +0900

    Update CalendarExample.java

--- sec10/CalendarExample.java
+++ sec10/CalendarExample.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class CalendarExample {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		Calendar now = Calendar.getInstance();
 		int year = now.get(Calendar.YEAR);
