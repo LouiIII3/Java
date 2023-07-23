887f1eb3f5a03ff71d7d4ae5ce553228367de234
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-23 15:23:02 +0900

    Update CalendarExample.java

--- sec10/CalendarExample.java
+++ sec10/CalendarExample.java
@@ -9,9 +9,11 @@ public class CalendarExample {
 		Calendar now = Calendar.getInstance();
 		int year = now.get(Calendar.YEAR);
 		int month = now.get(Calendar.MONTH) + 1;
+		
 		int day = now.get(Calendar.DAY_OF_MONTH);
 		int week = now.get(Calendar.DAY_OF_WEEK);
 		int amPm = now.get(Calendar.AM_PM);
+		
 		int hour = now.get(Calendar.HOUR);
 		int minute = now.get(Calendar.MINUTE);
 		int second = now.get(Calendar.SECOND);
