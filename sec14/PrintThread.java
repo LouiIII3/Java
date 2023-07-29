757cc87dd83e7be0ce239915e3c99b25b39e5918
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-07-29 09:08:42 +0900

    Add files via upload

---
+++ sec14/PrintThread.java
@@ -0,0 +1,13 @@
+package sec14.exam01;
+
+public class PrintThread extends Thread {
+	public void run() {	
+		try {
+			while(true) {
+				System.out.println("실행");
+				Thread.sleep(1);
+			}	
+		} catch(InterruptedException e) {}
+		System.out.println("종료");
+	}
+}
---
+++ sec14/XXXThread.java
@@ -0,0 +1,5 @@
+package sec14.exam01;
+
+public class XXXThread {
+
+}
