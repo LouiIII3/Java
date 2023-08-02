efea88b07b51d799dc7c5a227c83e94a3d950ee7
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-08-02 22:34:37 +0900

    Update PrintThread.java

--- sec14/PrintThread.java
+++ sec14/PrintThread.java
@@ -2,7 +2,6 @@ package sec14.exam01;
 
 public class PrintThread extends Thread {
 	public void run() {
-		
 		try {
 			while(true) {
 				System.out.println("실행");
@@ -10,6 +9,5 @@ public class PrintThread extends Thread {
 			}	
 		} catch(InterruptedException e) {}
 		System.out.println("종료");
-		
 	}
 }
