62050fb5a5465ebff87c05994e89b23eb84eda2f
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-08-25 11:48:49 +0900

    Update ChildExample2.java

--- sec07.exam01/ChildExample2.java
+++ sec07.exam01/ChildExample2.java
@@ -1,13 +1,14 @@
 package sec07.exam01;
 
+
 public class ChildExample2 {
 	public static void main(String[] args) {
 		Parent2 parent = new Child2();
-		parent.field1 = "data1"; //자동 타입 변환
+		parent.field1 = "data1"; //자동 타입 변환 예시
 		parent.method1();
 		parent.method2();
 		
-		Child2 child = (Child2) parent; //강제 타입 변환
+		Child2 child = (Child2) parent; //강제 타입 변환 예시
 		child.field2 = "yyy";
 		child.method3();
 		}
